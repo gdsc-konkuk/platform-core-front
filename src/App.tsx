d326a3b32ba8147b9d9c86cebc5b823a4442957e
MinboyKim <kdm3801@gmail.com>
2024-07-03 17:31:19 +0900

    Initial Configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <main>
+      <h1 className="text-4xl text-red-500">Hello World</h1>
+      <Button variant="destructive">Click me</Button>
+    </main>
+  );
+}
+
+export default App;
