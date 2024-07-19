45c4674ed8bc258114cdd511dee8fa3eb69d89ef
MinboyKim <kdm3801@gmail.com>
2024-07-19 16:54:08 +0900

    design: fonts
    Pretendard, SUITE 폰트 추가

--- src/features/main-layout/components/MainLayout.tsx
+++ src/features/main-layout/components/MainLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 export default function MainLayout() {
   return (
-    <div>
+    <div className="font-pretendard">
       <h1>Main Layout</h1>
       <Outlet />
     </div>
