a757a4f768428807902c3dd9f707592c20a74369
xoxun <00xxs2@naver.com>
2024-07-28 15:02:12 +0900

    feat: Attendance & Modal
    -출석 페이지 레이아웃 완성
    - 날짜 클릭 시 나오는 모달 창 레이아웃 완성
    - input number 디폴트 화살표 없애는 css 코드 추가

--- src/features/attendance/components/Attendance.tsx
+++ src/features/attendance/components/Attendance.tsx
@@ -1,95 +1,207 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import dayjs from 'dayjs';
 import localeData from 'dayjs/plugin/localeData';
+import GrayCloseIcon from '/icons/close-gray.svg';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+import { Input } from '@/components/ui/input';
 
 dayjs.extend(localeData);
 
 export default function Attendance() {
   const [currentDate, setCurrentDate] = useState(dayjs());
   const [selectedDate, setSelectedDate] = useState<dayjs.Dayjs | null>(null);
+  const [selectedYear, setSelectedYear] = useState(currentDate.year());
+  const [selectedMonth, setSelectedMonth] = useState(currentDate.month());
+  const borderRef = useRef<HTMLDivElement>(null);
+  const [title, setTitle] = useState('');
+  const [numberOfPeople, setNumberOfPeople] = useState(0);
 
   const handleDateClick = (date: dayjs.Dayjs) => {
     setSelectedDate(date);
   };
 
+  const handleYearChange = (value: string) => {
+    const newYear = Number(value);
+    setSelectedYear(newYear);
+    setCurrentDate(currentDate.year(newYear));
+  };
+
+  const handleMonthChange = (value: string) => {
+    const newMonth = Number(value);
+    setSelectedMonth(newMonth);
+    setCurrentDate(currentDate.month(newMonth));
+  };
   const daysInMonth = currentDate.daysInMonth();
   const firstDayOfMonth = currentDate.startOf('month').day();
 
   return (
-    <div className="p-4">
-      <div className="flex justify-between items-center mb-4">
-        <h1 className="font-['NanumSquareRoundEB'] text-[24px] font-extrabold">
+    <div className="px-4 flex flex-col items-center bg-[#ffffff]">
+      <div>
+        <h1 className="mb-[3vh] font-['NanumSquareRoundEB'] text-[24px] font-extrabold">
           출석
         </h1>
-        <div>
-          <select
-            value={currentDate.year()}
-            onChange={(e) =>
-              setCurrentDate(currentDate.year(Number(e.target.value)))
-            }
-            className="mr-2"
-          >
-            {Array.from({ length: 10 }, (_, i) => (
-              <option key={i} value={dayjs().year() - 5 + i}>
-                {dayjs().year() - 5 + i}
-              </option>
+        <div className="w-[1008px] pt-11 pl-14 pb-11 pr-11 border border-solid border-[#DADADA] rounded-3xl ">
+          <div className="flex justify-start items-center mb-4">
+            <Select onValueChange={handleYearChange}>
+              <SelectTrigger className="w-[110px] mr-2 text-[#171719] font-[Pretendard] text-[22px] font-[600]">
+                <SelectValue placeholder={selectedYear} />
+              </SelectTrigger>
+              <SelectContent className="text-[#171719] font-[Pretendard] text-[22px] font-[600]">
+                {Array.from({ length: 10 }, (_, i) => (
+                  <SelectItem
+                    key={i}
+                    value={(dayjs().year() - 5 + i).toString()}
+                  >
+                    {dayjs().year() - 5 + i}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Select onValueChange={handleMonthChange}>
+              <SelectTrigger className="w-[80px] mr-2 text-[#171719] font-[Pretendard] text-[22px] font-[600]">
+                <SelectValue placeholder={selectedMonth + 1} />
+              </SelectTrigger>
+              <SelectContent className="mr-2 text-[#171719] font-[Pretendard] text-[22px] font-[600]">
+                {Array.from({ length: 12 }, (_, i) => (
+                  <SelectItem key={i} value={i.toString()}>
+                    {i + 1}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div className="w-[full] h-[1px] border-t my-4"></div>
+          <div className="grid grid-cols-7 gap-2 ">
+            {['월', '화', '수', '목', '금', '토', '일'].map((day) => (
+              <div
+                key={day}
+                className="mb-[18px] text-start text-[#868687] text-[16px] font-[Pretendard] font-[600]"
+              >
+                {day}
+              </div>
             ))}
-          </select>
-          <select
-            value={currentDate.month()}
-            onChange={(e) =>
-              setCurrentDate(currentDate.month(Number(e.target.value)))
-            }
-          >
-            {dayjs()
-              .localeData()
-              .months()
-              .map((month, index) => (
-                <option key={index} value={index}>
-                  {month}
-                </option>
-              ))}
-          </select>
-        </div>
-      </div>
 
-      <div className="grid grid-cols-7 gap-2">
-        {['월', '화', '수', '목', '금', '토', '일'].map((day) => (
-          <div key={day} className="text-center font-bold">
-            {day}
-          </div>
-        ))}
+            {Array.from({ length: firstDayOfMonth - 1 }, (_, i) => (
+              <div key={i}></div>
+            ))}
 
-        {Array.from({ length: firstDayOfMonth }, (_, i) => (
-          <div key={i}></div>
-        ))}
-
-        {Array.from({ length: daysInMonth }, (_, i) => (
-          <div
-            key={i}
-            className="p-2 text-center cursor-pointer hover:bg-gray-200"
-            onClick={() => handleDateClick(currentDate.date(i + 1))}
-          >
-            {i + 1}
-          </div>
-        ))}
-      </div>
-
-      {selectedDate && (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-4 rounded">
-            <h2 className="text-lg font-bold">
-              {selectedDate.format('YYYY-MM-DD')}
-            </h2>
-            <button
-              onClick={() => setSelectedDate(null)}
-              className="mt-4 px-4 py-2 bg-gray-200"
-            >
-              Close
-            </button>
+            {Array.from({ length: daysInMonth }, (_, i) => (
+              <div
+                key={i}
+                className="w-[120px] h-[95px] px-[16px] py-[10px] rounded-[10px] cursor-pointer bg-[#F9F9F9] hover:bg-gray-200 transition duration-300"
+                onClick={() => handleDateClick(currentDate.date(i + 1))}
+              >
+                <span className="text-[#535355] font-[Pretendard] text-[20px] font-[600]">
+                  {i + 1}
+                </span>
+                {/* <div className="w-[11px] h-[11px] bg-[#9747FF] rounded-[11px]"></div>
+                <div className="w-[11px] h-[11px] bg-[#EA4335] rounded-[11px]"></div> */}
+              </div>
+            ))}
           </div>
         </div>
-      )}
+
+        {selectedDate && (
+          <div className="fixed inset-0 bg-gray-800 bg-opacity-10 flex justify-center items-center">
+            <div className="w-[70%] h-[100%] relative flex flex-col justify-center items-center bg-[#ffffff] p-4 rounded-[10px] filter drop-shadow-[0px_4px_10px_rgba(0,0,0,0.15)]">
+              <h2 className="top-[78px] left-[60px] absolute font-['NanumSquareRoundEB'] text-[24px] font-extrabold">
+                출석 QR 생성
+              </h2>
+              <form className="flex flex-col w-[400px] h-[422px] relative">
+                <label
+                  htmlFor="title"
+                  className="mb-[18px] text-[#5C5353] font-Pretendard] text-[17px] font-[600] leading-[1.5] tracking-[0.17px]"
+                >
+                  제목
+                </label>
+                <div className="relative w-full mb-[80px]">
+                  <Input
+                    disabled
+                    id="title"
+                    className="w-[400px] h-[50px] rounded-[10px] border-[#DADADA] bg-white px-[12px] text-[16px] focus:border-[#b1b1b1]"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    onFocus={() => {
+                      borderRef.current?.style.setProperty(
+                        'background-color',
+                        '#b1b1b1',
+                      );
+                    }}
+                    onBlur={() => {
+                      borderRef.current?.style.setProperty(
+                        'background-color',
+                        '#b1b1b1',
+                      );
+                    }}
+                  />
+                  <img
+                    src={GrayCloseIcon}
+                    alt="close"
+                    className="absolute right-4 top-[14px] cursor-pointer"
+                    onClick={() => setTitle('')}
+                  />
+                </div>
+                <label
+                  htmlFor="number"
+                  className="mb-[18px] text-[#5C5353] font-Pretendard] text-[17px] font-[600] leading-[1.5] tracking-[0.17px]"
+                >
+                  총 인원 수
+                </label>
+                <div className="relative w-full">
+                  <Input
+                    disabled
+                    type="number"
+                    id="number"
+                    className="w-[400px] h-[50px] rounded-[10px] border-[#DADADA] bg-white px-[12px] text-[16px] focus:border-[#b1b1b1]"
+                    value={numberOfPeople.toString()}
+                    onChange={(e) => setNumberOfPeople(Number(e.target.value))}
+                    onFocus={() => {
+                      borderRef.current?.style.setProperty(
+                        'background-color',
+                        '#b1b1b1',
+                      );
+                    }}
+                    onBlur={() => {
+                      borderRef.current?.style.setProperty(
+                        'background-color',
+                        '#b1b1b1',
+                      );
+                    }}
+                  />
+                  <img
+                    src={GrayCloseIcon}
+                    alt="close"
+                    className="absolute right-4 top-[14px] cursor-pointer"
+                    onClick={() => setNumberOfPeople(0)}
+                  />
+                </div>
+                <button
+                  onClick={() => setSelectedDate(null)}
+                  className="absolute bottom-[0px] right-[0px] px-[28px] py-[12px] rounded-[10px] bg-[#ECECED] text-[17px] font-[Pretendard] font-[600] text-[#BEBEBF]"
+                >
+                  확인
+                </button>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
+}
+
+{
+  /* 
+          <CreateQRModal
+          selectedDate={selectedDate}
+          title={setTitle}
+          setTitle={setTitle}
+        /> */
 }
---
+++ src/features/attendance/components/CreateQRModal.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CreateQRModal = () => {
+  return <div>CreateQRModal</div>;
+};
