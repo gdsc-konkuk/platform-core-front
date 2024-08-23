6bb46acfd8ae96194e5b07fca4fff7ed30f2cb1e
MinboyKim <kdm3801@gmail.com>
2024-08-23 21:29:50 +0900

    chore: delete query devtool
    쿼리 데브툴 삭제

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import { useRoutes } from 'react-router-dom';
 import routes from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useAuth } from '@/stores/AuthProvider';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from '@/components/ui/toaster';
 
 const queryClient = new QueryClient();
@@ -14,7 +13,6 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={true} />
       {routing}
       <Toaster />
     </QueryClientProvider>
